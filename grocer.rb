d4bcb161fecef6bfccd973be37f6f7493f2c43a3
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-02-22 21:48:50 -0500

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,8 +1,16 @@
 def consolidate_cart(cart)
   new_hash = {}
   cart.each do |item_hash|
-
+    item_hash.each do |item, info_hash|
+      if new_hash[item]
+        new_hash[item][:count] += 1
+      else
+        new_hash[item] = info_hash
+        new_hash[item][:count] = 1
+      end
+    end
   end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
